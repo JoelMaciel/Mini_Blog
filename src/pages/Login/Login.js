90fb4ef3817cb8ef0028539b30b2044d327e8a16
Joel Maciel <soldado25461@gmail.com>
2022-04-25 19:18:54 -0300

    estruturando pagina de logi

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,10 +1,59 @@
-import React from "react";
-import styles from "./Login.module.css"
+import React, { useEffect, useState } from "react";
+import { useAuthentication } from "../../hooks/useAuthentication";
+import styles from "./Login.module.css";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const { createUser, error: authError, loading } = useAuthentication();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+  };
+  useEffect(() => {
+    if (authError) {
+      setError(authError);
+    }
+  }, [authError]);
+
   return (
-    <div>
-      <h2>Login</h2>
+    <div className={styles.login}>
+      <h1>Login</h1>
+      <p>Faça o login para poder utilizar o sistemas</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>E-mail</span>
+          <input
+            type="email"
+            name="email"
+            required
+            placeholder="Digite seu email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label>
+          <span>Senha:</span>
+          <input
+            type="password"
+            name="password"
+            placeholder="Digite sua senha"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+
+        {!loading && <button className="btn">Entrar</button>}
+        {loading && (
+          <button className="btn" disabled>
+            Aguarde...
+          </button>
+        )}
+
+        {error && <p className="error">{error}</p>}
+      </form>
     </div>
   );
 };
