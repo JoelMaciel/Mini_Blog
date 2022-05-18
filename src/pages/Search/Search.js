833927d126db1689ded770a4c36c4b77dc2f368b
Joel Maciel <soldado25461@gmail.com>
2022-05-18 13:18:14 -0300

    erro search

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -1,13 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import PostDetail from "../../components/PostDetail";
+import { useFetchDocuments } from "../../hooks/useFetchDocument";
 import { useQuery } from "../../hooks/useQuery";
 
 const Search = () => {
   const query = useQuery();
   const search = query.get("q");
+  const { documents: posts } = useFetchDocuments("posts", search);
+
   return (
     <div>
       <h2>Search</h2>
-      <p>{search}</p>
+      {posts && posts.length === 0 && (
+        <>
+          <p>Não foram encontrados post a partir de sua busca</p>
+          <Link to="/">Voltar</Link>
+        </>
+      )}
+      {posts && posts.map((post) => <PostDetail key={post.id} post={post} />)}
     </div>
   );
 };
